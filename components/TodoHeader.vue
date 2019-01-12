fb171290a2ac6ab263c963fa5e72264f029f0098
huewilliams <4879sinwoo@naver.com>
2019-01-12 13:44:55 +0900

    CREATE: TodoHeader component

---
+++ components/TodoHeader.vue
@@ -0,0 +1,19 @@
+<template>
+  <header>
+    <h1>To-do List</h1>
+  </header>
+</template>
+
+<script>
+    export default {
+        name: "TodoHeader"
+    }
+</script>
+
+<style scoped>
+  ht {
+    color : #2F3B52;
+    font-weight : 900;
+    margin : 2.5rem 0 1.5rem;
+  }
+</style>
